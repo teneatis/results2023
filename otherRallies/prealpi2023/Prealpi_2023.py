eaeb77b9bdae2a63355d617165577f941dacc6c7
ilias <teneatis@hotmail.com>
2023-12-09 20:27:31 +0200

    endOfSeason

---
+++ otherRallies/prealpi2023/Prealpi_2023.py
@@ -0,0 +1,52 @@
+import urllib.request
+from bs4 import BeautifulSoup as soup
+#import requests
+import re
+import pandas as pd
+link = 'https://www.ewrc-results.com/results/82367-prealpi-master-show-2023/?s='
+startat, no_ss=443719, int(6)
+#monte_23 = pd.DataFrame(columns=['Pos.', 'Pilote / Co-pilote', 'Voiture', '#', 'Temps', 'Écart'])
+rally_23 = []
+canceled = []
+
+stages = [number for number in range(no_ss)]
+
+if canceled:
+    for j in canceled: stages.remove(j-1)
+
+
+#for ss in range(0,(no_ss)):
+for ss in stages:
+    val= startat + ss
+    ss_a = str(val)
+    my_url11 = link + ss_a
+    #print(my_url11)
+    req = urllib.request.Request(my_url11, headers={'User-Agent': 'Mozilla/5.0'})
+    uClient11 = urllib.request.urlopen(req)
+    page_html11 = uClient11.read()
+    uClient11.close()
+    data = pd.read_html(page_html11)[0]
+    data.columns=['Pos.', 'No', 'Crew', 'Gr/Cl','ss_time', 'Diff', 'Speed']
+    data['ss']=ss+1
+    equal = '=' in data['Pos.'].unique()
+    if equal:
+        data['Pos.'] = data['Pos.'].replace('=', method='ffill')
+        data['Pos.'] = data['Pos.'].astype(str).astype(float)
+    #print(data.dtypes)
+    #print(data)
+    rally_23.append(data) 
+
+
+rally2023_stages = pd.concat(rally_23, axis=0)
+rally2023_stages.columns=['Pos.', 'No', 'Crew', 'Gr/Cl','ss_time', 'Diff', 'Speed', 'ss']
+rally2023_stages['Pos.'] = rally2023_stages['Pos.'].astype(int)
+rally2023_stages.to_csv('Rally2023_st.csv', index=False)
+#print(rally2023_stages)
+#print(rally2023_stages.dtypes)
+
+dataview = rally2023_stages.drop(['Diff','Speed','Pos.'], axis=1)
+dataview = dataview.fillna('-')
+data_view2 = dataview.set_index(['No', 'Crew', 'Gr/Cl','ss'], drop=True).unstack('ss')
+data_view2 = data_view2.fillna('-')
+data_view2.to_csv('Rally2023_comp.csv')
+#print(data_view2)
